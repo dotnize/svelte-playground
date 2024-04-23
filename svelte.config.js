a5222e7a1e734e9956b0d106470fd96acb35de3e
Nathaniel Tampus <nate@mugnavo.com>
2024-04-23 20:25:37 +0800

    svelte 5 runes

--- svelte.config.js
+++ svelte.config.js
@@ -13,6 +13,10 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 	},
+
+	compilerOptions: {
+		runes: true,
+	},
 };
 
 export default config;
