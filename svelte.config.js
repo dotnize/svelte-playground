99f65c6a2b8c1f0baf413c0fde5ff3520dad2f7e
Nathaniel Tampus <nate@mugnavo.com>
2024-11-06 17:27:26 +0800

    remove forced runes config

--- svelte.config.js
+++ svelte.config.js
@@ -13,10 +13,6 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 	},
-
-	compilerOptions: {
-		runes: true,
-	},
 };
 
 export default config;
